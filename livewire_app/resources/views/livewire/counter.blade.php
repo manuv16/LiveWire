328e8cacfd4e91412d07d23324034632601c7325
manuv16 <manuella.v@escolar.ifrn.edu.br>
2023-07-19 16:08:33 -0300

    atualizacao

---
+++ livewire_app/resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div>
+    {{-- In work, do what you enjoy. --}}
+
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+</div>
--- livewire_app/routes/web.php
+++ livewire_app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Livewire\Counter;
+use Illuminate\Routing\RouteDependencyResolverTrait;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contador',\App\Http\Livewire\Counter::class);
